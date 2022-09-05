709cb2042ce9a1c4e6e008eef2c5fabdfe22f14b
Red <lebruitdered@gmail.com>
2022-09-05 09:40:29 +0200

    Movment

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,29 +5,74 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-
     [SerializeField] InputActionReference _move;
+    [SerializeField] InputActionReference _sprint;
+    [SerializeField] Rigidbody2D _rb;
+    [SerializeField] Animator _animator;
+
+    [SerializeField] float _speed;
+
+    Vector3 _direction;
+    Vector3 _aimDirection;
+    bool _isRunning;
 
     private void Start()
     {
         _move.action.started += StartMove;
-        _move.action.canceled += UpdateMove;
-        _move.action.performed += StopMove;
+        _move.action.performed += UpdateMove;
+        _move.action.canceled += StopMove;
+
+        //_sprint.action.started += StartSprint;
+        //_sprint.action.canceled += StopSprint;
+    }
+
+    private void StopSprint(InputAction.CallbackContext obj)
+    {
+        _isRunning = false;
+    }
+
+    private void StartSprint(InputAction.CallbackContext obj)
+    {
+        _isRunning = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log($"{_direction}");
+
+        _animator.SetFloat("Horizontal", _aimDirection.x);
+        _animator.SetFloat("Vertical", _aimDirection.y);
+        _animator.SetBool("IsMoving", _direction.magnitude > 0.1f);
+        _animator.SetBool("IsRunning", _isRunning);
+
+        _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed));
+
+
+        //if (_isRunning)
+        //{
+        //    _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed * 2));
+        //}
+        //else
+        //{
+        //    _rb.MovePosition(_rb.transform.position + (_direction * Time.fixedDeltaTime * _speed));
+        //}
 
     }
 
     private void StopMove(InputAction.CallbackContext obj)
     {
-
+        _direction = Vector3.zero;
     }
 
     private void UpdateMove(InputAction.CallbackContext obj)
     {
-
+        _direction = obj.ReadValue<Vector2>();
+        _aimDirection = _direction;
     }
 
     private void StartMove(InputAction.CallbackContext obj)
     {
-
+        _direction = obj.ReadValue<Vector2>();
+        _aimDirection = _direction;
     }
 }
